2167d8aafe0cdabfd68936acf39f11229076c694
liamraemclauchlan <liam.rae-mclauchlan@infinityworks.com>
2020-06-09 09:35:50 +0100

    Add extra test

--- test_count.py
+++ test_count.py
@@ -9,3 +9,11 @@ def test_count_1():
     total = count_vowels.count("aeiou")
     for vowel in total:
         assert total[vowel] == 1
+
+def test_count_x():
+    total = count_vowels.count("Hello World")
+    assert total["a"] == 0
+    assert total["e"] == 1
+    assert total["i"] == 0
+    assert total["o"] == 2
+    assert total["u"] == 0
